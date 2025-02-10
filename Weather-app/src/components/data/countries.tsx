940c5df7303ec62a220694d6708f874e162659a4
Gonzalo Agustín Reynoso <98622984+gonreynoso@users.noreply.github.com>
2025-02-10 18:13:53 -0300

    Eliminar README, agregar configuración de Prettier y Tailwind CSS, implementar FORM de búsqueda de clima. (#1)

---
+++ Weather-app/src/components/data/countries.tsx
@@ -0,0 +1,50 @@
+export const countries = [
+  {
+    name: "Argentina",
+    code: "AR",
+  },
+  {
+    name: "Bolivia",
+    code: "BO",
+  },
+  {
+    name: "Brasil",
+    code: "BR",
+  },
+  {
+    name: "Chile",
+    code: "CL",
+  },
+  {
+    name: "Colombia",
+    code: "CO",
+  },
+  {
+    name: "Ecuador",
+    code: "EC",
+  },
+  {
+    name: "Guyana",
+    code: "GY",
+  },
+  {
+    name: "Paraguay",
+    code: "PY",
+  },
+  {
+    name: "Peru",
+    code: "PE",
+  },
+  {
+    name: "Surinam",
+    code: "SR",
+  },
+  {
+    name: "Uruguay",
+    code: "UY",
+  },
+  {
+    name: "Venezuela",
+    code: "VE",
+  },
+];
